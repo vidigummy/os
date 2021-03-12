ff651f55cc881a5352e88eaaa4922c6732e4b02d
vidigummy <vidigummy@gmail.com>
2021-03-13 01:11:33 +0900

    test

--- week1/factorial.s
+++ week1/factorial.s
@@ -6,8 +6,16 @@ main:
     li $v0, 5
     syscall
     move $a0, $v0
+    addi $v0, $zero, 1
     addi $a1, $s2, 1
     jal factorial
+
+    move $a0, $v0
+    li $v0, 1
+    syscall
+
+    li $v0, 10
+    syscall
 
 factorial:
     bgt $a0, 0, recur
@@ -17,5 +25,18 @@ recur:
     addi $sp, $sp, -8
 
     sw $a0, 4($sp)
-    //밑바닥에 a0가져온 거 집어넣기
     sw $ra, 0($sp)
+
+    addi $a0, $a0, -1
+
+    jal factorial
+
+    lw $t0, 4($sp)
+
+    mul $v0, $v0, $t0
+
+    lw $ra, 0($sp)
+
+    addi $sp,$sp,8
+
+    jr $ra
